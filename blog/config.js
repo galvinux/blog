47851d79c02e24c8ad8b3332ad6746f318dab5b2
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2021-06-22 17:43:52 -0300

    First commit

---
+++ blog/config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://galvinux.github.io',
+  pathPrefix: '/',
+  title: 'Galvinux - Galvez Linux User Group',
+  subtitle: 'Grupo de usuarios de Gnu/Linux y Software Libre de la ciudad de Galvez, Santa Fe.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Artículos',
+      path: '/'
+    },
+    // {
+    //   label: 'About me',
+    //   path: '/pages/about'
+    // },
+    // {
+    //   label: 'Contact me',
+    //   path: '/pages/contacts'
+    // }
+  ],
+  author: {
+    name: 'Galvinux',
+    photo: '/photo.jpg',
+    bio: 'Grupo de usuarios de Gnu/Linux y Software Libre de la ciudad de Gálvez, Santa Fe.',
+    contacts: {
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: 'galvinux',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
---
+++ blog/content/posts/2021-22-06---Inauguracion-nuevo-grupo-linux-galvez.md
@@ -0,0 +1,12 @@
+---
+title: "Inauguración del grupo de Galvinux"
+date: "2020-06-22T22:12:03.284Z"
+template: "post"
+draft: false
+slug: "inauguracion-galvinux-grupo-usuarios-linux-galvez"
+category: "Anuncios"
+tags:
+  - "Anuncios"
+description: "German inventor Johannes Gutenberg developed a method of movable type and used it to create one of the western world’s first major printed books, the “Forty–Two–Line” Bible."
+socialImage: "/media/gutenberg.jpg"
+---
